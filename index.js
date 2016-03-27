1c7a19fc55b2b93b5ce1253a1a539ba2b58e0233
Claudio Mattioli (Coderonin) <seekereye@gmail.com>
2016-03-27 15:07:54 -0300

    add example files and read the block comments

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 var glob = require("glob"),
+	fs = require('fs'),
 	path = require('path'),
-	colors = require('colors');
+	colors = require('colors'),
+
+	//Variables
+
+	blockRegx = /\/\*\*(.|[\n\r])*?\*\//g,
+	lineRegx = /\*[^\/\*]*\b/g;
+
+function parseContent(fileName){
+	var data = fs.readFileSync(fileName, 'utf8'),
+		blocks = data.match(blockRegx);
+	return blocks || [];
+}
+
+function readDocumentationMetadata(block){
+	var lines = block.match(lineRegx);
+	return lines;
+}
 
 function docsBuilder(pattern, options){
-	var files = [];
+	var files = [],
+		blocks = [];
 
 	console.log("========================================================".green);
 	console.log("    Begin files reading...");
@@ -12,8 +30,24 @@ function docsBuilder(pattern, options){
 	glob.sync(pattern).forEach( function( file ) {
 		var fileName = path.resolve(file);
 		console.log(" -- Reading: ".green + fileName);
-		files.push(fileName);
+		blocks = blocks.concat(parseContent(fileName));
 	});
+
+	console.log("========================================================".green);
+	console.log("    Building references...");
+	console.log("    Blocks: " + blocks.length);
+	console.log("========================================================".green);
+
+	blocks.forEach(function(block){
+		var meta = readDocumentationMetadata(block);
+		meta.forEach(function(line){
+			console.log(line);
+		});
+	});
+
+	console.log("========================================================".green);
+	console.log("    Building references Complete...");
+	console.log("========================================================".green);
 
 	console.log("========================================================".green);
 	console.log("    Reading Complete...");
